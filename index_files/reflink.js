bd34693ac805320af0b0efe9950964b7c4be76c4
shulhaha <shulhaha@protonmail.com>
2023-07-05 23:47:27 +0300

    init

---
+++ index_files/reflink.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("a").click(function(){
+    $("#reflink*").attr("href", "https://promo.20bet.partners/redirect.aspx?pid=11112&bid=1971&lpid=281&sref=Link&Link=20bet_20bet522");
+  });
+});
